a09dfa62f93aeca7a4db1bd67c9bb65b7b85876e
armanmsb <arman-meshabi-ayandeh_student2021@wilder.school>
2021-09-21 15:03:04 +0200

    Java 08 by Arman

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,42 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.println("Took off");
+        this.flying = true;
+    }
+
+    @Override
+    public int ascend(int meters) {
+
+        if(isFlying()){
+            this.altitude = this.altitude + meters;
+            System.out.println(getName() + " is ascending to " + this.altitude + " meters");
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public int descend(int meters) {
+
+        if(isFlying()){
+            this.altitude = this.altitude - meters;
+            System.out.println(getName() + " is descending to " + this.altitude + " meters");
+        }
+        return this.altitude;
+
+    }
+
+    @Override
+    public void land() {
+        System.out.println(getName() + " is landing");
+    }
+
+    @Override
+    public void glide() {
+        Fly.super.glide();
     }
 }
